9f47d2c52ad3ae01135567df354cd29df59e57cf
Eric Busch <ericbusch@gmail.com>
2021-01-30 13:42:51 -0500

    Update toc.js

--- toc.js
+++ toc.js
@@ -42,7 +42,7 @@ document.addEventListener('DOMContentLoaded', function () {
         $heading.appendChild($anchor);
     });
 
-    if (document.getElementById('toc').hasChildNodes()) {
+    if (!document.getElementById('toc').hasChildNodes()) {
         document.getElementById('toc').remove();
     } else {
         $label = document.createElement('div');
